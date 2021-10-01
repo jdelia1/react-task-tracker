0d16ea7030294d5fdada089ea58856ab6bbc4d0d
Joe <joe.delia1993@gmail.com>
2021-10-01 18:59:23 -0400

    fix console error for enumerating without a dedicated key

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa'
 const Task = ({ task, onDelete, onToggle }) => {
     return (
         <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
-            <h3 key={task.id}>
+            <h3>
                 {task.text}
                 <FaTimes color='red' cursor='pointer' onClick={() => onDelete(task.id)} />
             </h3>
