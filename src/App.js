35949b6a5915dbdd683bdb9b090a380cac6b6fbf
Joe <joe.delia1993@gmail.com>
2021-10-01 19:23:27 -0400

    toggle add task display

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddTask from './components/AddTask'
 import {useState} from 'react'
 
 function App() {
+    const [showAddTask, setShowAddTask] = useState(false)
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -45,8 +46,8 @@ function App() {
 
     return (
         <div className="container">
-            <Header />
-            <AddTask onAdd={addTask} />
+            <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+            {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 ?
                 <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> :
                 <p>No Tasks</p>}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
 
-    const onClick = (e) => {
-        console.log(e)
-    }
-    
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add Task'} onClick={onAdd} />
         </header>
     )
 }
